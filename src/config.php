60de5a043742b4c346d87af9b8e3a5f482b941ee
sebastian <sebastian@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2015-05-08 20:17:04 +0000

    ZP-572 #comment set max default contact picture size to 5 MB
    git-svn-id: https://z-push.org/svn/z-push/trunk@1943 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- src/config.php
+++ src/config.php
@@ -191,8 +191,8 @@
     // in the semantic sanity checks and contacts with larger photos are not synchronized.
     // This limitation is not being followed by the ActiveSync clients which set much bigger
     // contact photos. You can override the default value of the max photo size.
-    // default: 49152 - 48 KB default max photo size in bytes
-    define('SYNC_CONTACTS_MAXPICTURESIZE', 49152);
+    // default: 5242880 - 5 MB default max photo size in bytes
+    define('SYNC_CONTACTS_MAXPICTURESIZE', 5242880);
 
     // Over the WebserviceUsers command it is possible to retrieve a list of all
     // known devices and users on this Z-Push system. The authenticated user needs to have
