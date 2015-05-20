d81adfa494b371b80289e3699fd08601a3a7cef3
sebastian <sebastian@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2015-05-20 18:16:34 +0000

    ZP-611 #comment if a folder has no name, set it to "Unknown"
    git-svn-id: https://z-push.org/svn/z-push/trunk@1947 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- src/lib/syncobjects/syncfolder.php
+++ src/lib/syncobjects/syncfolder.php
@@ -62,7 +62,7 @@ class SyncFolder extends SyncObject {
                                                                                     self::STREAMER_CHECKS   => array(   self::STREAMER_CHECK_REQUIRED   => self::STREAMER_CHECK_SETZERO)),
 
                     SYNC_FOLDERHIERARCHY_DISPLAYNAME                    => array (  self::STREAMER_VAR      => "displayname",
-                                                                                    self::STREAMER_CHECKS   => array(   self::STREAMER_CHECK_REQUIRED   => self::STREAMER_CHECK_SETEMPTY)),
+                                                                                    self::STREAMER_CHECKS   => array(   self::STREAMER_CHECK_REQUIRED   => "Unknown")),
 
                     SYNC_FOLDERHIERARCHY_TYPE                           => array (  self::STREAMER_VAR      => "type",
                                                                                     self::STREAMER_CHECKS   => array(   self::STREAMER_CHECK_REQUIRED   => 18,
