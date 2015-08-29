aac92fc789f1508fcb69c0581f9f9ea731077fae
Francisco Miguel Biete <fbiete@gmail.com>
2015-08-29 18:34:10 +0200

    ZP-671 Changes recommended by Manfred. Released under the Affero GNU General Public License (AGPL) version 3.

--- src/index.php
+++ src/index.php
@@ -298,12 +298,5 @@ include_once('version.php');
         ZPush::GetDeviceManager()->Save();
 
     // end gracefully
-    if (version_compare(phpversion(), '5.4.0') < 0) {
-        $time_used = number_format(time() - $_SERVER["REQUEST_TIME"], 4);
-    }
-    else {
-        $time_used = number_format(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"], 4);
-    }
-
-    ZLog::Write(LOGLEVEL_DEBUG, sprintf("-------- End - max mem: %s/%s - time: %s - code: %s", memory_get_peak_usage(false), memory_get_peak_usage(true), $time_used, http_response_code()));
+    ZLog::WriteEnd();
 ?>
