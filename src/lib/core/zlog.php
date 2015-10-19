57d97d62392ac48c05847b0dca0250c778510857
Sebastian Kummer <sebastian@z-push.org>
2015-10-19 14:03:40 -0200

    Revert "Merge pull request #64 in ZP/z-push from bugfix/ZP-720-user-level-log-won-t-writeend-if-global to develop"
    This reverts commit 8e916a4e1d05b0d4418d2d9aa9421e4cbe024673.

--- src/lib/core/zlog.php
+++ src/lib/core/zlog.php
@@ -174,7 +174,7 @@ class ZLog {
      * @return
      */
     static public function WriteEnd() {
-        if (LOGLEVEL_DEBUG <= LOGLEVEL || (LOGLEVEL_DEBUG <= LOGUSERLEVEL && self::$userLog)) {
+        if (LOGLEVEL_DEBUG <= LOGLEVEL) {
             if (version_compare(phpversion(), '5.4.0') < 0) {
                 $time_used = number_format(time() - $_SERVER["REQUEST_TIME"], 4);
             }
