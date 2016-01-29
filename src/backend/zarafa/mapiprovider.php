66a7ae5d1707af57871e484536bcd711dc824b15
Manfred Kutas <manfred@z-push.org>
2016-01-29 16:40:22 -0200

    ZP-817 Set task as private if sensitivity is private or greater.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/backend/zarafa/mapiprovider.php
+++ src/backend/zarafa/mapiprovider.php
@@ -1589,7 +1589,7 @@ class MAPIProvider {
             $recurrence->setRecurrence($recur);
         }
 
-        $props[$taskprops["private"]] = (isset($task->sensitivity) && $task->sensitivity == SENSITIVITY_PRIVATE) ? true : false;
+        $props[$taskprops["private"]] = (isset($task->sensitivity) && $task->sensitivity >= SENSITIVITY_PRIVATE) ? true : false;
 
         //open addresss book for user resolve to set the owner
         $addrbook = $this->getAddressbook();
