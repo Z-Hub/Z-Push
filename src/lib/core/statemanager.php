f7fbe8e63a50de8f4964749b2a69462e58fcbd7c
Sebastian Kummer <sebastian@z-push.org>
2016-03-11 15:13:56 -0300

    ZP-858 Ping fully reloads the hierarchy data before verifying hierarchy notifications, Ping has to wait before verifying to give other processes time to write all pending states.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/lib/core/statemanager.php
+++ src/lib/core/statemanager.php
@@ -129,15 +129,17 @@ class StateManager {
     /**
      * Returns a folder state (SyncParameters) for a folder id
      *
-     * @param $folderid
+     * @param string    $folderid
+     * @param boolean   $fromCacheIfAvailable   if set to false, the folderdata is always reloaded, default: true
      *
      * @access public
      * @return SyncParameters
      */
-    public function GetSynchedFolderState($folderid) {
+    public function GetSynchedFolderState($folderid, $fromCacheIfAvailable = true) {
         // new SyncParameters are cached
-        if (isset($this->synchedFolders[$folderid]))
+        if ($fromCacheIfAvailable && isset($this->synchedFolders[$folderid])) {
             return $this->synchedFolders[$folderid];
+        }
 
         $uuid = $this->device->GetFolderUUID($folderid);
         if ($uuid) {
