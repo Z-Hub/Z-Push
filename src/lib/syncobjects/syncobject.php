f84b01f001144b40a66a2927264e682a9adb2bcd
Manfred Kutas <manfred@z-push.org>
2016-06-06 12:15:23 -0300

    ZP-953 Print human friendly strings for integer values.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/lib/syncobjects/syncobject.php
+++ src/lib/syncobjects/syncobject.php
@@ -246,18 +246,21 @@ abstract class SyncObject extends Streamer {
                             $out[$keyprefix.$val] = Utils::GetFormattedTime($this->$val);
                         }
                         else {
-                            $out[$keyprefix.$val] = (isset($this->$val) && strlen($this->$val) ?
+                            $out[$keyprefix.$val] = (strlen($this->$val) ?
                                     Utils::GetFormattedTime($this->$val):"undefined") ." - ". $odoName .": ".
-                                    (isset($odo->$val) && strlen($odo->$val) ? Utils::GetFormattedTime($odo->$val) : "undefined");
+                                    (strlen($odo->$val) ? Utils::GetFormattedTime($odo->$val) : "undefined");
                         }
                     }
-                    // else just compare their values
+                    // else just compare their values and print human friendly if necessary
                     else {
                         if($this->$val == $odo->$val) {
-                            $out[$keyprefix.$val] = $this->$val;
+                            $out[$keyprefix.$val] = (isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val;
                         }
                         else {
-                            $out[$keyprefix.$val] = (isset($this->$val) && $this->$val ? $this->$val:"undefined") ." - ". $odoName .": ". (isset($odo->$val) && $odo->$val ? $odo->$val:"undefined");
+                            $out[$keyprefix.$val] = (strlen($this->$val) ?
+                                    ((isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val) : "undefined") .
+                                    " - ". $odoName .": ".
+                                    (strlen($odo->$val) ? ((isset($v[self::STREAMER_VALUEMAP][$odo->$val])) ? $v[self::STREAMER_VALUEMAP][$odo->$val] : $odo->$val) : "undefined");
                         }
                     }
                 }
@@ -272,7 +275,7 @@ abstract class SyncObject extends Streamer {
                 else {
                     if($this->$val == $odo->$val) {
                         if (! ($this instanceof SyncRecurrence)) {
-                            $out[$keyprefix.$val] = $this->$val;
+                            $out[$keyprefix.$val] = (isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val;
                         }
                     }
                     else {
@@ -280,7 +283,10 @@ abstract class SyncObject extends Streamer {
                             $out["Recurrence"] = "Recurrence changed";
                         }
                         else {
-                            $out[$keyprefix.$val] = (strlen($this->$val) ? $this->$val:"undefined") ." - ". $odoName .": ". (strlen($odo->$val) ? $odo->$val:"undefined");
+                            $out[$keyprefix.$val] = (strlen($this->$val) ?
+                                    ((isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val) : "undefined") .
+                                    " - ". $odoName .": ".
+                                    (strlen($odo->$val) ? ((isset($v[self::STREAMER_VALUEMAP][$odo->$val])) ? $v[self::STREAMER_VALUEMAP][$odo->$val] : $odo->$val) : "undefined");
                         }
                     }
                 }
@@ -291,7 +297,9 @@ abstract class SyncObject extends Streamer {
                 // Otherwise it's a ghosted property and the device didn't send it, so we don't have to care about that case.
                 if (in_array($k, $supportedFields)) {
                     if ((is_scalar($this->$val) && strlen($this->$val)) || (!is_scalar($this->$val) && !empty($this->$val))) {
-                        $out[$keyprefix.$val] = (is_array($this->$val) ? implode(",", $this->$val) : $this->$val) . " - " . $odoName .": value completely removed";
+                        $out[$keyprefix.$val] = (is_array($this->$val) ?
+                                implode(",", $this->$val) : ((isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val)) .
+                                " - " . $odoName .": value completely removed";
                     }
                 }
                 // there is no data sent for SyncMail, so just output its values
@@ -300,7 +308,7 @@ abstract class SyncObject extends Streamer {
                         $out[$keyprefix.$val] = Utils::GetFormattedTime($this->$val);
                     }
                     else {
-                        $out[$keyprefix.$val] = $this->$val;
+                        $out[$keyprefix.$val] = (isset($v[self::STREAMER_VALUEMAP][$this->$val])) ? $v[self::STREAMER_VALUEMAP][$this->$val] : $this->$val;
                     }
                 }
             }
@@ -314,7 +322,7 @@ abstract class SyncObject extends Streamer {
                         $out[$keyprefix.$val] = "Not set - " . $odoName . ": an exception was added";
                     }
                     else {
-                        $out[$keyprefix.$val] = "Not set - " . $odoName . ": " . $odo->$val . " (value added)";
+                        $out[$keyprefix.$val] = "Not set - " . $odoName . ": " . ((isset($v[self::STREAMER_VALUEMAP][$odo->$val])) ? $v[self::STREAMER_VALUEMAP][$odo->$val] : $odo->$val) . " (value added)";
                     }
                 }
                 else if (isset($v[self::STREAMER_ARRAY])) {
@@ -322,7 +330,7 @@ abstract class SyncObject extends Streamer {
                     $out[$keyprefix.$val] = "Not set - ". $odoName .": ". implode(", ", $odo->$val) . " (value added)";
                 }
                 else {
-                    $out[$keyprefix.$val] = "Not set - " . $odoName . ": " . $odo->$val . " (value added)";
+                    $out[$keyprefix.$val] = "Not set - " . $odoName . ": " . ((isset($v[self::STREAMER_VALUEMAP][$odo->$val])) ? $v[self::STREAMER_VALUEMAP][$odo->$val] : $odo->$val) . " (value added)";
                 }
             }
         }
