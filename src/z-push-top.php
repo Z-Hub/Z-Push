140cd642defd6f234d3975bf66a1ab335b8ca823
Felix Bartels (Zarafa) <f.bartels@kopano.com>
2016-07-13 12:31:53 +0200

    ZP-985 Packages for Red Hat Software collections make actually no use of them. Released under the Affero GNU General Public License (AGPL) version 3.
    make use of the same php-mapi packages as webapp does on scl distributions
    change shebang of php cli script to make use of env (the php binary is in a different location for these distributions as well)

--- src/z-push-top.php
+++ src/z-push-top.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+#!/usr/bin/env php
 <?php
 /***********************************************
 * File      :   z-push-top.php
--- tools/migrate-filestates-to-db.php
+++ tools/migrate-filestates-to-db.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+#!/usr/bin/env php
 <?php
 /**********************************************************
 * File      :   migrate-filestates-to-db.php
