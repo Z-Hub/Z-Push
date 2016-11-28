487f3f36e07a97c2671b3f87f60bac5f8e44ed7f
Sebastian Kummer <sebastian@z-push.org>
2016-11-28 13:39:30 -0200

    ZP-1103 redirect error output of 'hash git' to /dev/null.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/version.php
+++ src/version.php
@@ -25,8 +25,8 @@
 
 if (!defined("ZPUSH_VERSION")) {
     $path = escapeshellarg(dirname(realpath($_SERVER['SCRIPT_FILENAME'])));
-    $branch = trim(exec("hash git && cd $path >/dev/null 2>&1 && git branch --no-color  2>/dev/null | sed -e '/^[^*]/d' -e \"s/* \(.*\)/\\1/\""));
-    $version = exec("hash git && cd $path >/dev/null 2>&1 && git describe  --always &2>/dev/null");
+    $branch = trim(exec("hash git 2>/dev/null && cd $path >/dev/null 2>&1 && git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e \"s/* \(.*\)/\\1/\""));
+    $version = exec("hash git 2>/dev/null && cd $path >/dev/null 2>&1 && git describe  --always 2>/dev/null");
     if ($branch && $version) {
         define("ZPUSH_VERSION", $branch .'-'. $version);
     }
