5cc71129fa08c65cc11261a79c8e5921054a463f
Sebastian Kummer <sebastian@z-push.org>
2017-06-26 11:11:57 -0300

    ZP-1240 Add HTML safe truncation options to stream wrappers.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/lib/utils/stringstreamwrapper.php
+++ src/lib/utils/stringstreamwrapper.php
@@ -30,6 +30,7 @@ class StringStreamWrapper {
     private $stringstream;
     private $position;
     private $stringlength;
+    private $truncateHtmlSafe;
 
     /**
      * Opens the stream
@@ -53,9 +54,10 @@ class StringStreamWrapper {
 
         // this is our stream!
         $this->stringstream = $contextOptions[self::PROTOCOL]['string'];
+        $this->truncateHtmlSafe = (isset($contextOptions[self::PROTOCOL]['truncatehtmlsafe'])) ? $contextOptions[self::PROTOCOL]['truncatehtmlsafe'] : false;
 
         $this->stringlength = strlen($this->stringstream);
-        ZLog::Write(LOGLEVEL_DEBUG, sprintf("StringStreamWrapper::stream_open(): initialized stream length: %d", $this->stringlength));
+        ZLog::Write(LOGLEVEL_DEBUG, sprintf("StringStreamWrapper::stream_open(): initialized stream length: %d - HTML-safe-truncate: %s", $this->stringlength,  Utils::PrintAsString($this->truncateHtmlSafe)));
 
         return true;
     }
@@ -135,7 +137,7 @@ class StringStreamWrapper {
      */
     public function stream_truncate ($new_size) {
         // cut the string!
-        $this->stringstream = Utils::Utf8_truncate($this->stringstream, $new_size);
+        $this->stringstream = Utils::Utf8_truncate($this->stringstream, $new_size, $this->truncateHtmlSafe);
         $this->stringlength = strlen($this->stringstream);
 
         if ($this->position > $this->stringlength) {
@@ -161,13 +163,14 @@ class StringStreamWrapper {
    /**
      * Instantiates a StringStreamWrapper
      *
-     * @param string    $string     The string to be wrapped
+     * @param string    $string             The string to be wrapped
+     * @param boolean   $truncatehtmlsafe   Indicates if a truncation should be done html-safe - default: false
      *
      * @access public
      * @return StringStreamWrapper
      */
-     static public function Open($string) {
-        $context = stream_context_create(array(self::PROTOCOL => array('string' => &$string)));
+     static public function Open($string, $truncatehtmlsafe = false) {
+        $context = stream_context_create(array(self::PROTOCOL => array('string' => &$string, 'truncatehtmlsafe' => $truncatehtmlsafe)));
         return fopen(self::PROTOCOL . "://",'r', false, $context);
     }
 }
