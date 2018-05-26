Z-Hub/Z-Push

commit 7563cdbc8818216194045998d792757f2a6c8417

ZP-1418 IMAP Backend: imap_fetch_overview might return an empty array. Released under the Affero GNU General Public License (AGPL) version 3.
Fixes sync issues due to:
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1636 Undefined offset: 0 (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1637 Undefined offset: 0 (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1637 Trying to get property of non-object (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1639 Undefined offset: 0 (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1639 Trying to get property of non-object (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1641 Undefined offset: 0 (8)
> 07/05/2018 07:32:41 [18309] [WARN] [test@example.com] /usr/share/z-push/backend/imap/imap.php:1641 Trying to get property of non-object (8)

src/backend/imap/imap.php
@@ -957,7 +957,7 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
         ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendIMAP->GetMessageList(): searching with sequence '%s'", $sequence));
         $overviews = @imap_fetch_overview($this->mbox, $sequence);
 
-        if (!is_array($overviews)) {
+        if (!is_array($overviews) || count($overview) == 0) {
             $error = imap_last_error();
             if (strlen($error) > 0 && imap_num_msg($this->mbox) > 0) {
                 ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->GetMessageList('%s','%s'): Failed to retrieve overview: %s", $folderid, $cutoffdate, imap_last_error()));
@@ -1603,7 +1603,7 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
             // read message flags
             $overview = @imap_fetch_overview($this->mbox, $id, FT_UID);
 
-            if (!is_array($overview)) {
+            if (!is_array($overview) || count($overview) == 0) {
                 throw new StatusException(sprintf("BackendIMAP->MoveMessage('%s','%s','%s'): Error, unable to retrieve overview of source message: %s", $folderid, $id, $newfolderid, imap_last_error()), SYNC_MOVEITEMSSTATUS_INVALIDSOURCEID);
             }
             else {

commit 9faf8678a0d5a5aab902ad40509389fe790c4665

ZP-1418 Fix missing s in $overviews. Released under the Affero GNU General Public License (AGPL) version 3.
This typo was introduced in 7563cdbc8818216194045998d792757f2a6c8417.

src/backend/imap/imap.php
@@ -957,7 +957,7 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
         ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendIMAP->GetMessageList(): searching with sequence '%s'", $sequence));
         $overviews = @imap_fetch_overview($this->mbox, $sequence);
 
-        if (!is_array($overviews) || count($overview) == 0) {
+        if (!is_array($overviews) || count($overviews) == 0) {
             $error = imap_last_error();
             if (strlen($error) > 0 && imap_num_msg($this->mbox) > 0) {
                 ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->GetMessageList('%s','%s'): Failed to retrieve overview: %s", $folderid, $cutoffdate, imap_last_error()));