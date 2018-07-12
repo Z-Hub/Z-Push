06fa912301259af51b3ce5c20a036c158ed7a9c1
Manfred Kutas <manfred@z-push.org>
2018-07-12 11:09:26 -0300

    ZP-1444 KOE webservices calls fail for impersonated stores.
    Released under the Affero GNU General Public License (AGPL) version 3.

--- src/backend/kopano/kopano.php
+++ src/backend/kopano/kopano.php
@@ -1666,7 +1666,7 @@ class BackendKopano implements IBackend, ISearchProvider {
         $storeProps = mapi_getprops($this->store, array(PR_MESSAGE_SIZE_EXTENDED));
         $storesize = isset($storeProps[PR_MESSAGE_SIZE_EXTENDED]) ? $storeProps[PR_MESSAGE_SIZE_EXTENDED] : 0;
 
-        $userDetails = $this->GetUserDetails($this->mainUser);
+        $userDetails = $this->GetUserDetails($this->impersonateUser ?: $this->mainUser);
         $userStoreInfo->SetData($foldercount, $storesize, $userDetails['fullname'], $userDetails['emailaddress']);
         ZLog::Write(LOGLEVEL_DEBUG, sprintf("KopanoBackend->GetUserStoreInfo(): user %s (%s) store size is %d bytes and contains %d folders",
                 Utils::PrintAsString($userDetails['fullname']), Utils::PrintAsString($userDetails['emailaddress']), $storesize, $foldercount));
