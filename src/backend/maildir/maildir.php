2b88587f8de2b702b616182802aebf344eb2b26d
bob4os <54377409+bob4os@users.noreply.github.com>
2022-06-26 11:58:05 +0200

    PHP8  and additional header "Importance"
    Replaced curly braces for array (PHP8 compatibility) and added the "Importance" header in addition to "X-Priority" in the "maildir" backend

--- src/backend/maildir/maildir.php
+++ src/backend/maildir/maildir.php
@@ -279,7 +279,7 @@ class BackendMaildir extends BackendDiff {
             return false;
 
         while($entry = readdir($dir)) {
-            if($entry{0} == ".")
+            if($entry[0] == ".")
                 continue;
 
             $message = array();
@@ -383,15 +383,36 @@ class BackendMaildir extends BackendDiff {
             }
         }
 
-        // convert mime-importance to AS-importance
-        if (isset($message->headers["x-priority"])) {
-            $mimeImportance =  preg_replace("/\D+/", "", $message->headers["x-priority"]);
-            if ($mimeImportance > 3)
-                $output->importance = 0;
-            if ($mimeImportance == 3)
+        // convert mime-importance to AS-importance using RFC4021, X-Priority or default to "normal" (ZP-320)
+        //AS: 0 - low, 1 - normal, 2 - important
+        if (isset($message->headers["importance"])) {
+            //Importance: high, normal, low
+            $mimeImportance = strtolower($message->headers["importance"]);
+            if ($mimeImportance == "normal") {
                 $output->importance = 1;
-            if ($mimeImportance < 3)
+            }
+            elseif ($mimeImportance == "high") {
                 $output->importance = 2;
+            }
+            elseif ($mimeImportance == "low") {
+                $output->importance = 0;
+            }
+        }
+        elseif (isset($message->headers["x-priority"])) {
+            //X-Priority: 1 - highest, 2 - high, 3 - normal, 4 - low, 5 - lowest
+            $mimeImportance = preg_replace("/\D+/", "", $message->headers["x-priority"]);
+            if ($mimeImportance == 3) {
+                $output->importance = 1;
+            }
+            elseif ($mimeImportance < 3) {
+                $output->importance = 2;
+            }
+            elseif ($mimeImportance > 3) {
+                $output->importance = 0;
+            }
+        }
+        else {
+            $output->importance = 1;
         }
 
         // Attachments are only searched in the top-level part
@@ -679,7 +700,7 @@ class BackendMaildir extends BackendDiff {
         $newdir = opendir($newdirname);
 
         while($newentry = readdir($newdir)) {
-            if($newentry{0} == ".")
+            if($newentry[0] == ".")
                 continue;
 
             // link/unlink == move. This is the way to move the message according to cr.yp.to
