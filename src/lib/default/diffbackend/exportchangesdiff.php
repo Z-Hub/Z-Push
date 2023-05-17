d76a9ab293f6c95aa86a86149616e129ce4437d6
matidau <65836048+matidau@users.noreply.github.com>
2023-05-17 18:10:37 +1000

    Check if $message returns false
    Released under the GNU Affero General Public License (AGPL), version 3.

--- src/lib/default/diffbackend/exportchangesdiff.php
+++ src/lib/default/diffbackend/exportchangesdiff.php
@@ -160,7 +160,8 @@ class ExportChangesDiff extends DiffState implements IExportChanges{
                         $message = $this->backend->GetMessage($this->folderid, $change["id"], $this->contentparameters);
 
                         // copy the flag to the message
-                        $message->flags = (isset($change["flags"])) ? $change["flags"] : 0;
+                        if($message)
+                            $message->flags = (isset($change["flags"])) ? $change["flags"] : 0;
 
                         if($stat && $message) {
                             if($this->flags & BACKEND_DISCARD_DATA || $this->importer->ImportMessageChange($change["id"], $message) == true)
