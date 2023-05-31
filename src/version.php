fc62b3d7692be635532d0a162f84e3c66d71a75e
matidau <65836048+matidau@users.noreply.github.com>
2023-05-31 16:22:26 +1000

    Update version.php
    To include version when not coming from a git clone. such as Mail in a boxes use. Need a text file named version in the same directory as version.php

--- src/version.php
+++ src/version.php
@@ -27,8 +27,13 @@ if (!defined("ZPUSH_VERSION")) {
     $path = escapeshellarg(dirname(realpath($_SERVER['SCRIPT_FILENAME'])));
     $branch = trim(exec("hash git 2>/dev/null && cd $path >/dev/null 2>&1 && git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e \"s/* \(.*\)/\\1/\""));
     $version = exec("hash git 2>/dev/null && cd $path >/dev/null 2>&1 && git describe  --always 2>/dev/null");
+    $file = dirname(realpath($_SERVER['SCRIPT_FILENAME'])) . '/version';
     if ($branch && $version) {
         define("ZPUSH_VERSION", $branch .'-'. $version);
+    }
+    elseif (file_exists($file)) {
+        $line = fgets(fopen($file, 'r'));
+        define("ZPUSH_VERSION", $line);
     }
     else {
         define("ZPUSH_VERSION", "GIT");
